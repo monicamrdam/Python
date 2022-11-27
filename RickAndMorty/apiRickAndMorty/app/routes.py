d4df4bfcd4c2cd73da370b9e335e2a8abf7cfcbc
monicamrdam <107017946+monicamrdam@users.noreply.github.com>
2022-11-27 03:54:32 +0100

    Update routes.py

--- RickAndMorty/apiRickAndMorty/app/routes.py
+++ RickAndMorty/apiRickAndMorty/app/routes.py
@@ -3,6 +3,7 @@ from flask import Flask
 #Importamos la libreria jsonify para convertir un objeto en un json
 from flask import jsonify
 
+from .controller import list_all_characters
 
 #Creamos la aplicación de servidor
 app = Flask(__name__)
@@ -12,3 +13,9 @@ app = Flask(__name__)
 def home():
     return '!Bienvenido!'
 
+
+#Importamos los datos de los character que hemos obtenido de la API
+@app.route('/characters')
+def getCharacters():
+    return list_all_characters
+    #return jsonify(list_all_characters())
